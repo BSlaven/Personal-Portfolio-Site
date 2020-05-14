e598fe28a1e0b55fa9592e4f34f7e7c175bb251d
Slaven B <slaven.bunijevac@gmail.com>
2020-05-14 12:05:35 +0200

    Prvi unos

---
+++ slaven.js
@@ -0,0 +1,11 @@
+// const link = document.querySelector('.za_github');
+
+const otvoriMeni = document.querySelector('#otvori-meni');
+const navbar = document.querySelector('#navbar');
+const lista = document.querySelector('#nav-list');
+const header = document.querySelector('#header');
+
+otvoriMeni.addEventListener('click', () => {
+  lista.classList.toggle('prikaži-meni');
+  header.classList.toggle('prikaži-meni');
+});
