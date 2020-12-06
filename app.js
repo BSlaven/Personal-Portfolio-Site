c1b20db72c6a5feaecdc63fd69bd4dd7b60732d7
Slaven B <slaven.bunijevac@gmail.com>
2020-12-06 22:01:34 +0100

    correct spelling in commit message

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const showNavbar = document.querySelector('#show-navbar');
 const navbar = document.querySelector('#navbar');
 const container = document.querySelector('.container');
 
+
 showNavbar.addEventListener('click', () => {
   navbar.classList.toggle('visible-navbar');
   showNavbar.classList.toggle('navbar-open');
