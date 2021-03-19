6d3fe903f6c2e841f86b86bbd6917430c41ed804
Slaven B <slaven.bunijevac@gmail.com>
2021-03-20 00:28:54 +0100

    change font front page and add ui to top button

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const showNavbar = document.querySelector('#show-navbar');
 const navbar = document.querySelector('#navbar');
 const container = document.querySelector('.container');
+const toTopBtn = document.querySelector('#back-to-top');
+const main = document.querySelector('#main');
+
+// document.addEventListener('scroll', scrollDocumentToTop);
+// window.onscroll = function() {scrollDocumentToTop()};
+
+
+function scrollDocumentToTop() {
+
+  // if(document.body.scrollTop > 100) {
+  //   toTopBtn.style.display = 'block';
+  // } else {
+  //   toTopBtn.style.display = 'none'
+  // }
+  console.log(document.scrollingElement.scrollTop);
+}
+
+toTopBtn.addEventListener('click', () => {
+  console.log('Slaven ide na vrh');
+})
+
 
 
 showNavbar.addEventListener('click', () => {
